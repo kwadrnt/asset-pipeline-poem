6bfbd02ac02e3bbaa6364dc17dadc8b2200a1dfa
kwadrnt <kevinctse@gmail.com>
2017-05-25 16:43:33 -0700

    modified app.js and css for to render on localhost

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,7 +3,7 @@
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
 // or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
+
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // compiled file.
 //
@@ -12,4 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
+//= require title
+//= require roses_are_red
+//= require violets_are_blue
+//= require compile_your_assets
+//= require then_hit_opt_cmd_u
+//= require poem_meta_data
